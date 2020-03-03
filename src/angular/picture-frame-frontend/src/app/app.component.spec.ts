004e6a5346ebe73cd31a3dc9752f448062421ece
dennis <dennis.reger@floppyfist.com>
2020-03-03 22:20:45 +0100

    Clean up generated ng project

--- src/angular/picture-frame-frontend/src/app/app.component.spec.ts
+++ src/angular/picture-frame-frontend/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'picture-frame-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('picture-frame-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('picture-frame-frontend app is running!');
-  });
 });

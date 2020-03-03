d8447d51bdf1b16cad9c1ff4742f7c4c8e69caee
dennis <dennis.reger@floppyfist.com>
2020-03-03 23:15:46 +0100

    Implement Tabs

--- src/angular/picture-frame-frontend/src/app/app.module.ts
+++ src/angular/picture-frame-frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatTabsModule} from '@angular/material/tabs';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

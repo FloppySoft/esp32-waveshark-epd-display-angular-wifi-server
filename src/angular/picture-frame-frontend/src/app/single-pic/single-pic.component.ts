f2d33e6a4328ed4f4230781146809807bbe3d940
dennis <dennis.reger@floppyfist.com>
2020-03-09 21:16:19 +0100

    Implement canvas-based image processing

--- src/angular/picture-frame-frontend/src/app/single-pic/single-pic.component.ts
+++ src/angular/picture-frame-frontend/src/app/single-pic/single-pic.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+
+export interface Vector2D { // TODO: Move reusable interface into root
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-single-pic',
@@ -6,9 +11,14 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./single-pic.component.scss'],
 })
 export class SinglePicComponent implements OnInit {
+  @ViewChild('editCanvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
+  screenSize = { // TODO: Move into global config singleton
+    x: 400,
+    y: 300,
+  };
   localUrl = null;
   isDragging = false;
-
   constructor() { }
 
   ngOnInit(): void {
@@ -32,27 +42,61 @@ export class SinglePicComponent implements OnInit {
     const reader = new FileReader();
     reader.onload = (event) => {
       this.localUrl = event.target.result;
-      // this.render(this.localUrl);
+      this.render(this.localUrl);
     },
       reader.readAsDataURL(file);
   }
 
-
   render(src) {
     const image = new Image();
     image.onload = () => {
-      const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
-      /*     if (image.height > this.MAX_HEIGHT) {
-            image.width *= this.MAX_HEIGHT / image.height;
-            image.height = this.MAX_HEIGHT;
-          } */
-      const ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      canvas.width = image.width;
-      canvas.height = image.height;
-      ctx.drawImage(image, 0, 0, image.width, image.height);
+      const size: Vector2D = { // Size of input image
+        x: image.width,
+        y: image.height,
+      };
+      const target: Vector2D = { // Target size (of this project's display)
+        x: this.screenSize.x,
+        y: this.screenSize.y,
+      };
+      const clipped: Vector2D = this.coverCrop(size, target); // Crop image to aspect ratio
+      const offset = {  // Put image in middle
+        x: (size.x - clipped.x) / 2,
+        y: (size.y - clipped.y) / 2,
+      };
+      this.ctx = this.canvas.nativeElement.getContext('2d');
+      this.canvas.nativeElement.width = target.x;
+      this.canvas.nativeElement.height = target.y;
+      this.ctx.drawImage(
+        image,              // img    - Image to draw on canvas
+        offset.x,           // sx     - Offset position, if aspect ratio not fitting
+        offset.y,           // sy
+        clipped.x,          // swidth - Width of cropped selection on original
+        clipped.y,          // sheight
+        0,                  // x      - Draw position
+        0,                  // y
+        target.x,  // width  - Drawing width on canvas
+        target.y,  // heigt
+      );
     };
     image.src = src;
+  }
+
+  private coverCrop(size: Vector2D, target: Vector2D): Vector2D {
+    // Handle aspect ratio of input like a object-fit: cover
+    const targetRatio = target.x / target.y;
+    if (size.x / size.y > targetRatio) {
+      // Wider than target image
+      return ({
+        x: size.y * (targetRatio),
+        y: size.y,
+      });
+    } else {
+      // Taller than target image (or fitting)
+      return ({
+        x: size.x,
+        y: size.x / (targetRatio),
+      });
+    }
   }
 
 }
